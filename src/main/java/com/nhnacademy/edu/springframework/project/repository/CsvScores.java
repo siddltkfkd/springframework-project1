42e5c44630ba1b1a689f3136022a8060a2516d5b
dmswl <siddltkfkd@gmail.com>
2024-02-01 20:34:05 +0900

    todo2 singleton 패턴으로 CsvScores 구현

--- src/main/java/com/nhnacademy/edu/springframework/project/repository/CsvScores.java
+++ src/main/java/com/nhnacademy/edu/springframework/project/repository/CsvScores.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CsvScores implements Scores {
+    private static final CsvScores INSTANCE = new CsvScores();
 
     private CsvScores(){}
 
@@ -11,7 +12,7 @@ public class CsvScores implements Scores {
      * Java Singleton 패턴으로 getInstance() 를 구현하세요.
      **/
     public static Scores getInstance() {
-        return null;
+        return INSTANCE;
     }
 
     // TODO 5 : score.csv 파일에서 데이터를 읽어 멤버 변수에 추가하는 로직을 구현하세요.
