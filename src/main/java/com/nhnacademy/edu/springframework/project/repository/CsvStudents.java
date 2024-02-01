63b21706c773706074f0365ad360774878ef3eec
dmswl <siddltkfkd@gmail.com>
2024-02-01 20:42:22 +0900

    todo3 singleton 패턴으로 CsvStudents 구현

--- src/main/java/com/nhnacademy/edu/springframework/project/repository/CsvStudents.java
+++ src/main/java/com/nhnacademy/edu/springframework/project/repository/CsvStudents.java
@@ -8,13 +8,16 @@ import java.util.Collection;
 
 
 public class CsvStudents implements Students {
+    private static final CsvStudents INSTANCE = new CsvStudents();
 
+    private CsvStudents() {
+    }
 
     /** TODO 3 :
      * Java Singleton 패턴으로 getInstance() 를 구현하세요.
      **/
     public static Students getInstance() {
-        return null;
+        return INSTANCE;
     }
 
     // TODO 7 : student.csv 파일에서 데이터를 읽어 클래스 멤버 변수에 추가하는 로직을 구현하세요.
