01fd7a5e5734279f3e15e98e4bbf4492cdd7437a
dmswl <siddltkfkd@gmail.com>
2024-02-01 21:28:11 +0900

    todo5 CsvScores - score.csv 파일을 읽어서 멤버변수에 추가

--- src/main/java/com/nhnacademy/edu/springframework/project/repository/CsvScores.java
+++ src/main/java/com/nhnacademy/edu/springframework/project/repository/CsvScores.java
@@ -1,12 +1,17 @@
 package com.nhnacademy.edu.springframework.project.repository;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class CsvScores implements Scores {
     private static final CsvScores INSTANCE = new CsvScores();
+    private List<Score> loadData;
 
-    private CsvScores(){}
+    private CsvScores(){
+        loadData = new ArrayList<>();
+    }
 
     /** TODO 2 :
      * Java Singleton 패턴으로 getInstance() 를 구현하세요.
@@ -18,11 +23,32 @@ public class CsvScores implements Scores {
     // TODO 5 : score.csv 파일에서 데이터를 읽어 멤버 변수에 추가하는 로직을 구현하세요.
     @Override
     public void load() {
-
+        File csv = new File("/home/nhnacademy/IdeaProjects/springframework-project1/src/main/resources/data/score.csv");
+        BufferedReader br = null;
+        String line;
+        try{
+            br = new BufferedReader(new FileReader(csv));
+            while((line = br.readLine()) != null){
+                String[] lineSplit = line.split(",");
+                loadData.add(new Score(Integer.valueOf(lineSplit[0]), Integer.valueOf(lineSplit[1])));
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try{
+                if(Objects.nonNull(br)){
+                    br.close();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     @Override
     public List<Score> findAll() {
-        return null;
+        return loadData;
     }
 }
