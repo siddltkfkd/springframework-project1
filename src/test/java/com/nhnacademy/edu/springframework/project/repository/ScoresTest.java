1f77b09b28d4f6bbf086dc9716549b035b19a908
dmswl <siddltkfkd@gmail.com>
2024-02-02 13:44:31 +0900

    ScoresTest 작성

--- src/test/java/com/nhnacademy/edu/springframework/project/repository/ScoresTest.java
+++ src/test/java/com/nhnacademy/edu/springframework/project/repository/ScoresTest.java
@@ -1,6 +1,10 @@
 package com.nhnacademy.edu.springframework.project.repository;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.csv.Csv;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,9 +12,21 @@ class ScoresTest {
 
     @Test
     void load() {
+        Scores csvScores = CsvScores.getInstance();
+        csvScores.load();
+        List<Score> scores = csvScores.findAll();
+        Assertions.assertEquals(scores.size(), 15);
     }
 
     @Test
     void findAll() {
+        Scores csvScores = CsvScores.getInstance();
+        List<Score> scores = csvScores.findAll();
+        Assertions.assertEquals(scores.size(),0);
+
+        csvScores.load();
+        scores = csvScores.findAll();
+        Assertions.assertEquals(scores.size(), 15);
     }
+
 }
