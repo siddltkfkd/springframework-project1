ad86f2953a3f55afb6683e83bb8324043e35a31b
dmswl <siddltkfkd@gmail.com>
2024-02-02 13:07:39 +0900

    todo7 CsvStudents - load()

--- src/main/java/com/nhnacademy/edu/springframework/project/repository/CsvStudents.java
+++ src/main/java/com/nhnacademy/edu/springframework/project/repository/CsvStudents.java
@@ -2,15 +2,19 @@ package com.nhnacademy.edu.springframework.project.repository;
 
 import com.nhnacademy.edu.springframework.project.service.Student;
 
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Collection;
-
-
+import java.util.List;
+import java.util.Objects;
 
 
 public class CsvStudents implements Students {
     private static final CsvStudents INSTANCE = new CsvStudents();
+    private List<Student> loadData;
 
     private CsvStudents() {
+        loadData = new ArrayList<>();
     }
 
     /** TODO 3 :
@@ -24,7 +28,28 @@ public class CsvStudents implements Students {
     // 데이터를 적재하고 읽기 위해서, 적절한 자료구조를 사용하세요.
     @Override
     public void load() {
-
+        File csv = new File("/home/nhnacademy/IdeaProjects/springframework-project1/src/main/resources/data/student.csv");
+        BufferedReader br = null;
+        String line;
+        try{
+            br = new BufferedReader(new FileReader(csv));
+            while((line = br.readLine()) != null){
+                String[] lineSplit = line.split(",");
+                loadData.add(new Student(Integer.valueOf(lineSplit[0]), lineSplit[1]));
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try{
+                if(Objects.nonNull(br)){
+                    br.close();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     @Override
